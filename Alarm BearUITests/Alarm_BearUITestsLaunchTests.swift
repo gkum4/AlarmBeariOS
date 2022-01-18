b4869df6c033aa317ad038a71809f6aaa65b0e53
gkum4 <gustavokumasawa@gmail.com>
2022-01-18 10:54:35 -0300

    Initial Commit

---
+++ Alarm BearUITests/Alarm_BearUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Alarm_BearUITestsLaunchTests.swift
+//  Alarm BearUITests
+//
+//  Created by Gustavo Kumasawa on 18/01/22.
+//
+
+import XCTest
+
+class Alarm_BearUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
