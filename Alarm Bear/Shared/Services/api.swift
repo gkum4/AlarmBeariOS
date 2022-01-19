347b552de3ccc92936e5d0b138599c867fd07336
gkum4 <gustavokumasawa@gmail.com>
2022-01-18 21:24:34 -0300

    Project basic structure

---
+++ Alarm Bear/Shared/Services/api.swift
@@ -0,0 +1,8 @@
+//
+//  api.swift
+//  Alarm Bear
+//
+//  Created by Gustavo Kumasawa on 18/01/22.
+//
+
+import Foundation
--- Alarm Bear/Sources/Scenes/Main/View/Controller/MainViewController.swift
+++ Alarm Bear/Sources/Scenes/Main/View/Controller/MainViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
